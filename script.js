b67996a434541072ced91f23b0aafd68b32f43c0
Evan Holloway <evan.holloway@alaskaair.com>
2024-02-01 09:01:59 -0800

    finishing exercise

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // Write your code here
+const bulb1 = document.querySelector("#lightbulb1");
+const bulb2 = document.querySelector("#lightbulb2");
+const bulb3 = document.querySelector("#lightbulb3");
+const countStr = document.querySelector(".subtitle");
+let count = 0;
+
+bulb1.addEventListener("click", function() {
+    count++;
+    countStr.innerHTML = "You've clicked the lights " + count + " times";
+    bulb1.classList.toggle("active");
+});
+bulb2.addEventListener("click", function() {
+    count++;
+    countStr.innerHTML = "You've clicked the lights " + count + " times";
+    bulb2.classList.toggle("active");
+});
+bulb3.addEventListener("click", function() {
+    count++;
+    countStr.innerHTML = "You've clicked the lights " + count + " times";
+    bulb3.classList.toggle("active");
+});
